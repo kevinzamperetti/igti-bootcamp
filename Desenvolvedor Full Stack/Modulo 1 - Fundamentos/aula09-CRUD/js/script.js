d1a8a4c3f683516d0bca9bab4d772fe097a5f938
Kevin Schepke <kevin.schepke@softplan.com.br>
2020-09-25 17:00:49 -0300

    Início da Aula 9 de CRUD

--- Desenvolvedor Full Stack/Modulo 1 - Fundamentos/aula09-CRUD/js/script.js
+++ Desenvolvedor Full Stack/Modulo 1 - Fundamentos/aula09-CRUD/js/script.js
@@ -39,16 +39,47 @@ function activateinput() {
 }
 
 function render(){
+  function createDeleteButton(index) {
+    function deleteName() {
+      globalNames.splice(index, 1);
+      render();
+    }
+
+    var button = document.createElement('button');
+    button.classList.add('delete-button');
+    button.textContent = 'X';
+
+    button.addEventListener('click', deleteName);
+
+    return button;
+  }
+
   var divNames = document.querySelector('#names');
+  divNames.innerHTML = '';
+
   var ul = document.createElement('ul');
+
   for(var i = 0; i < globalNames.length; i++) {
     var currentName = globalNames[i];
 
     var li = document.createElement('li');
-    li.textContent = currentName;
+    var button = createDeleteButton(i);
+
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = currentName;
+
+    li.appendChild(button);
+    li.appendChild(span);
+
     ul.appendChild(li);
   }
+
   divNames.appendChild(ul);
+  clearInput();
 }
 
-//Parei na aula 9.2
+function clearInput() {
+  globalInputName.value = '';
+  globalInputName.focus();
+}

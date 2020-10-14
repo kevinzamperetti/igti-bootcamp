2786a062049c3f5b28704be37fe19b08071542e9
Kevin Schepke <kevin.schepke@softplan.com.br>
2020-10-14 00:31:44 -0300

    Criação dos métodos GET, POST, PUT, PATCH e DELETE

--- Desenvolvedor Full Stack/Modulo 2 - Desenvolvimento APIs/modulo-express/my-bank-api/index.js
+++ Desenvolvedor Full Stack/Modulo 2 - Desenvolvimento APIs/modulo-express/my-bank-api/index.js
@@ -3,25 +3,23 @@ import accountRouter from "./routes/account.js";
 import {promises as fs} from "fs";
 
 const {readFile, writeFile} = fs;
+global.fileName = "accounts.json";
 
 const app = express();
 app.use(express.json());
-
-//
-
 app.use("/account", accountRouter);
 
 //start api
 app.listen(3000, async () => {
     try {
-        await readFile("accounts.json");
+        await readFile(global.fileName);
         console.log("API iniciada.");
     } catch (error) {
         const initialJson = {
             nextId: 1,
             accounts: []
         };
-        writeFile("accounts.json", JSON.stringify(initialJson))
+        writeFile(global.fileName, JSON.stringify(initialJson))
         .then(() => console.log("API iniciada and File Created."))
         .catch(err => console.log(err))
     }
--- Desenvolvedor Full Stack/Modulo 2 - Desenvolvimento APIs/modulo-express/my-bank-api/routes/account.js
+++ Desenvolvedor Full Stack/Modulo 2 - Desenvolvimento APIs/modulo-express/my-bank-api/routes/account.js
@@ -1,5 +1,93 @@
 import express from "express";
+import { promises as fs, read } from "fs";
+
+const { readFile, writeFile } = fs;
 
 const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        
+        account = { id: data.nextId, ...account } //coloca id como primeiro e pego demais atributos
+        data.nextId++;  //incremento o id
+
+        data.accounts.push(account);  //incluo novo registro no accounts.json existente
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));  //escrevo novo accounts.json
+
+        res.send(account);  //retorno o usuário salvo com o id gerado
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/", async (req, res, next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        delete data.nextId;  //retira o atributo nextId do accounts.json
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/:id", async (req, res,next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        const account = data.accounts.find(account => account.id === parseInt(req.params.id));
+        res.send(account);
+    } catch (error) {
+        next(err);
+    }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        data.accounts = data.accounts.filter(account => account.id !== parseInt(req.params.id)); //retorna array sem o que eu quero remover
+        writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.end();
+    } catch (error) {
+        next(err);
+    }
+});
+
+router.put("/", async (req, res, next) => {
+    try {
+        let account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        const index = data.accounts.findIndex(acc => acc.id === account.id);
+
+        data.accounts[index] = account;
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+        res.send(account);
+    } catch (error) {
+        next(err);
+    }
+});
+
+router.patch("/update-balance", async (req, res, next) => {
+    try {
+        let account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName));  //ler accounts.json com dados que já tenho
+        const index = data.accounts.findIndex(acc => acc.id === account.id);
+
+        data.accounts[index].balance = account.balance;
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+        res.send(data.accounts[index]);
+    } catch (error) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) => {
+    console.log(err);
+    res.status(400).send({ error: err.message })
+})
 
 export default router;
